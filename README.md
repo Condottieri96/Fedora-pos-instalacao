cf07fab3f51efc6ee4635d3b91603329f42fec74
Thales Martim Coutinho <thales9696@protonmail.com>
2022-08-31 11:50:30 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Fedora-pos-instala-o
+# Fedora-pos-instalacao
 script para automatizar configurações e instalações de meu ambiente de trabalho
+
+sintasse a vontade para pega-lo e adaptalo incluindo os softwares de sua preferencia e retirnado os que não utiliza
---
+++ fedora-pos-instalacao.sh
@@ -0,0 +1,139 @@
+#!/usr/bin/env bash
+
+#--------------PERSOLANALIZAÇÕES------------------------------------------------------#
+# mudando para o tema escuro
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+#desabilita "hot-corners" no gnome
+gsettings set org.gnome.desktop.interface enable-hot-corners false
+
+#atalho de teclado
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+
+# Keybind 0
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "<Control><Alt>t"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "gnome-terminal"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "Terminal"
+
+#----------------------ADD FLATHUB----------------------------------------------------#
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+#-------------------INICIO COMANDOS ROOT----------------------------------------------#
+sudo -s <<EOF
+#----------------DNF------------------------------------------------------------------#
+dnf update -y
+
+# adicionando repositirios
+dnf install -y \
+https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+
+# removendo programas que não utilizo
+dnf remove -y \
+cheese \
+rhythmbox \
+totem-video-thumbnailer \
+gnome-maps \
+gnome-tour \
+gnome-boxes \
+gnome-photos \
+gnome-contacts \
+gnome-connections \
+libreoffice*
+
+dnf autoremove -y
+
+#-----------------------INSTALAÇÔES ---------------------------------------------------#
+
+#programas DNF
+dnf install -y \
+vlc \
+rclone \
+toolbox
+
+# KVM para virtualizações
+dnf install -y @virtualization && \
+systemctl start libvirtd && \
+systemctl enable libvirtd
+
+#programas FLATPAK
+flatpak install -y \
+com.github.tchx84.Flatseal \
+com.obsproject.Studio \
+md.obsidian.Obsidian \
+org.libreoffice.LibreOffice \
+org.qbittorrent.qBittorrent \
+org.remmina.Remmina \
+io.github.spacingbat3.webcord \
+org.keepassxc.KeePassXC
+EOF
+#------------------FINAL COMANDOS ROOT------------------------------------------------##!/usr/bin/env bash
+
+#--------------PERSOLANALIZAÇÕES------------------------------------------------------#
+# mudando para o tema escuro
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+#desabilita "hot-corners" no gnome
+gsettings set org.gnome.desktop.interface enable-hot-corners false
+
+#atalho de teclado
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+
+# Keybind 0
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "<Control><Alt>t"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "gnome-terminal"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "Terminal"
+
+#----------------------ADD FLATHUB----------------------------------------------------#
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+#-------------------INICIO COMANDOS ROOT----------------------------------------------#
+sudo -s <<EOF
+#----------------DNF------------------------------------------------------------------#
+dnf update -y
+
+# adicionando repositirios
+dnf install -y \
+https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+
+# removendo programas que não utilizo
+dnf remove -y \
+cheese \
+rhythmbox \
+totem-video-thumbnailer \
+gnome-maps \
+gnome-tour \
+gnome-boxes \
+gnome-photos \
+gnome-contacts \
+gnome-connections \
+libreoffice*
+
+dnf autoremove -y
+
+#-----------------------INSTALAÇÔES ---------------------------------------------------#
+
+#programas DNF
+dnf install -y \
+vlc \
+rclone \
+toolbox
+
+# KVM para virtualizações
+dnf install -y @virtualization && \
+systemctl start libvirtd && \
+systemctl enable libvirtd
+
+#programas FLATPAK
+flatpak install -y \
+com.github.tchx84.Flatseal \
+com.obsproject.Studio \
+md.obsidian.Obsidian \
+org.libreoffice.LibreOffice \
+org.qbittorrent.qBittorrent \
+org.remmina.Remmina \
+io.github.spacingbat3.webcord \
+org.keepassxc.KeePassXC
+EOF
+#------------------FINAL COMANDOS ROOT------------------------------------------------#
